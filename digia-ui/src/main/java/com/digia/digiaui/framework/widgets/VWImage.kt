b3ac1bdaf6fbfbd615c9cc4830004f68a57036ec
Arun kumar <arunkumar@Aruns-MacBook-Air.local>
2026-01-15 18:39:02 +0530

    fixing the fillmaxsize issue

--- digia-ui/src/main/java/com/digia/digiaui/framework/widgets/VWImage.kt
+++ digia-ui/src/main/java/com/digia/digiaui/framework/widgets/VWImage.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
